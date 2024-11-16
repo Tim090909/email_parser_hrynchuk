fe189237baf5550629c532703cf2f75d72126bc1
Tim090909 <t.hrynchuk@ukma.edu.ua>
2024-11-16 13:45:07 +0200

    added  documentation for structs and enums

--- src/lib.rs
+++ src/lib.rs
@@ -2,23 +2,39 @@ use pest::Parser;
 use pest_derive::Parser;
 use thiserror::Error;
 
+
+/// The `EmailParser` is a parser generated using the `pest` library
+/// and the grammar defined in the `grammar.pest` file.
 #[derive(Parser)]
 #[grammar = "./grammar.pest"]
 pub struct EmailParser;
 
+/// Represents a structured email, including headers, body, and extracted email addresses.
 #[derive(Debug)]
 pub struct ParsedEmail {
+    /// A vector of key-value pairs representing the headers of the email.
+    /// Each header is represented as a `(String, String)` tuple.
     pub headers: Vec<(String, String)>,
+    
+    /// The main body of the email as a string.
     pub body: String,
+    
+    /// A list of email addresses extracted from the headers or body.
     pub email_addresses: Vec<String>,
 }
 
+/// Enum representing possible errors that might occur during email parsing.
 #[derive(Debug, Error)]
 pub enum EmailParseError {
+    /// A general parsing error, providing a descriptive message.
     #[error("Parsing error occurred: {0}")]
     ParseError(String),
+    
+    /// Indicates an invalid email address was encountered.
     #[error("Invalid email address: {0}")]
     InvalidEmailAddress(String),
+    
+    /// Indicates that a required field is missing in the email.
     #[error("Missing required field: {0}")]
     MissingField(String),
 }
@@ -46,12 +62,8 @@ impl ParsedEmail {
                                 return Err(EmailParseError::InvalidEmailAddress(value));
                             }
                             email_addresses.push(value.clone());
-                        } else {
-                            if (!name.is_empty() && value.is_empty())
-                                || (name.is_empty() && !value.is_empty())
-                            {
-                                return Err(EmailParseError::MissingField(name));
-                            }
+                        } else if (!name.is_empty() && value.is_empty()) || (name.is_empty() && !value.is_empty()){
+                            return Err(EmailParseError::MissingField(name));
                         }
 
                         headers.push((name, value));
